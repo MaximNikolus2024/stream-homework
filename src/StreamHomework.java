9595b208fb66a1f0f8754616ca14d0724249d637
Maxim Nikolus <nikolusmaxim409@gmail.com>
2024-07-01 17:26:25 +0200

    add homework

---
+++ src/StreamHomework.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamHomework {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(15);
+        list.add(40);
+        list.add(20);
+        list.add(200);
+        list.add(35);
+        list.add(187);
+        list.add(10);
+        list.add(100);
+        list.add(117);
+        list.add(113);
+        list.add(220);
+        list.add(48565);
+    }
+}
